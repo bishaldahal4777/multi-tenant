a5ed63ee0e0e5bfea18f1a1bedf4b46a8366f5af
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-01 08:25:04 +0545

    admin

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def task_list(request):
+    if not request.tenant:
+        return render(request, "error.html", {"message": "Tenant not found"})
