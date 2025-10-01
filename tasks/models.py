e38a4d225e98847be33c9df4732a148aebc974e7
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-01 07:48:29 +0545

    multi tenant project

---
+++ tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Tenant(models.Model):
+    name= models.CharField(max_length=100, unique=True)
+    domain = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class tasks(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"[{self.tenant.name}] {self.title}"
